e7939ca21b076d6a5075e5d52e70969bf8be896c
staubjo <67969701+jostaub@users.noreply.github.com>
2022-12-14 22:25:42 +0100

    use vim-plugin and easymotion

---
+++ .config/nvim/README.md
@@ -0,0 +1,4 @@
+# General Infos
+
+## Setup
+- install [vim-plug](https://github.com/junegunn/vim-plug)
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,19 @@
 require('settings')
 require('keybinds')
 
+-- Plugins
+local Plug = vim.fn['plug#']
+vim.call("plug#begin")
+
 if vim.g.vscode then
- require('vscode')
+    Plug("asvetliakov/vim-easymotion", {as = "vsc-easymotion"})
+else
+    Plug("easymotion/vim-easymotion")
+end
+
+vim.call("plug#end")
+
+-- load vscode specific settings
+if vim.g.vscode then 
+    require('vscode')
 end
