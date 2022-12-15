93431b305afedd84df52b78c6ad39a14a56c8cc2
staubjo <67969701+jostaub@users.noreply.github.com>
2022-12-15 21:43:16 +0100

    small refactor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,15 +6,17 @@ require('keybinds')
 local Plug = vim.fn['plug#']
 vim.call("plug#begin")
 
-if vim.g.vscode then
-    Plug("asvetliakov/vim-easymotion", {as = "vsc-easymotion"})
-else
+if not vim.g.vscode then
+    -- plugins if neovim is not used in vscode
     Plug("easymotion/vim-easymotion")
+else
+    -- plugins if neovim is used in vscode
+    Plug("asvetliakov/vim-easymotion", {as = "vsc-easymotion"})
 end
 
 vim.call("plug#end")
 
--- load vscode specific settings
+-- load vscode specific settings if needed
 if vim.g.vscode then 
     require('vscode')
 end
